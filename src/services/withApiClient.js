e5837489ce2842c81dbf991b955be9101f61dadf
Vanessacor <vanessapcbarros@gmail.com>
2020-06-04 11:46:57 +0200

    change apiclient

--- src/services/withApiClient.js
+++ src/services/withApiClient.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { ApiClient } from "./apiClient";
 
-const apiClient = new ApiClient("http://localhost:8000");
+const apiClient = new ApiClient("https://pure-garden-69207.herokuapp.com");
 
 export const withApiClient = (Component) => {
   return (props) => <Component apiClient={apiClient} {...props} />;
